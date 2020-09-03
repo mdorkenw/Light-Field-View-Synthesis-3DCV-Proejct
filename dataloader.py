1dacc36c2bb802777b97d9e00262971f318ce18b
valentinwust <44555270+valentinwust@users.noreply.github.com>
2020-09-03 16:49:29 +0200

    Add random crop, remove rot/flip
    Added random crop, removed rotation and flip, will add them correctly soon.
    Changed training directory name from "training" to "train".

--- dataloader.py
+++ dataloader.py
@@ -18,62 +18,54 @@ class RandomRightAngleRotation:
 class dataset(torch.utils.data.Dataset):
     def __init__(self, opt, mode='train'):
 
-        self.img_path = opt.Paths['img_path']
         self.img_size = opt.Network['image_size']
         self.mode = mode
-
+        
+        self.length = int(opt.Training[mode+'_size'])
+        self.img_path = opt.Paths['img_path'] + mode +'/'
+        self.scenes = os.listdir(self.img_path)
+        
         if mode == 'train':
-            self.length   = int(opt.Training['train_size'])
-            self.scenes   = os.listdir(self.img_path + 'training/')
-            self.img_path = opt.Paths['img_path'] + 'training/'
+            self.transform = transforms.Compose([
+                 transforms.RandomCrop(self.img_size),
+                 transforms.ColorJitter(brightness=0.4, contrast=0.5, saturation=0.4, hue=0.15),
+                 
+                 transforms.ToTensor(),
+                 transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            ])
         elif mode == 'test':
-            self.length = int(opt.Training['test_size'])
-            self.scenes   = os.listdir(self.img_path + mode)
-            self.img_path = opt.Paths['img_path'] + 'test/'
-
-        self.augment_train = transforms.Compose([
-             transforms.Resize(self.img_size),
-             transforms.RandomHorizontalFlip(p=0.5),
-             RandomRightAngleRotation(),
-             transforms.ColorJitter(brightness=0.4, contrast=0.5, saturation=0.4, hue=0.15),
-             transforms.ToTensor(),
-             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-        ])
-
-        self.augment_test  = transforms.Compose([
-            transforms.Resize(self.img_size),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-        ])
-
-    def get_1hot_(self, label):
-        hot1 = np.zeros(self.n_classes)
-        hot1[label] = 1
-        return hot1
+            self.transform  = transforms.Compose([
+                transforms.RandomCrop(self.img_size),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            ])
+        else:
+            raise NameError('Wrong mode!')
 
     def load_img(self, data_path, seed):
         img = Image.open(data_path)
-        if self.mode == 'train':
-            random.seed(seed)
-            return self.augment_train(img)
-        else:
-            return self.augment_test(img)
+        random.seed(seed)
+        return self.transform(img)
+    
+    def get_image_path(self, scene, image):
+        return self.img_path + self.scenes[scene] + "/" + 'input_Cam' + str(image).zfill(3) + ".png"
 
     def __getitem__(self, idx):
 
         seed = np.random.randint(2147483647)
 
         rand_scene = np.random.randint(0, len(self.scenes))
-        choice  = np.random.choice(3, 1)[0]
 
-        x1 = [self.load_img(self.img_path + self.scenes[rand_scene] + "/" + 'input_Cam' + str(i + 4).zfill(3)
-                            + ".png", seed) for i in range(0, 81, 9)]
-        x2 = [self.load_img(self.img_path + self.scenes[rand_scene] + "/" + 'input_Cam' + str(i + 36).zfill(3)
-                            + ".png", seed) for i in range(9)]
-        x3 = [self.load_img(self.img_path + self.scenes[rand_scene] + "/" + 'input_Cam' + str(i * 10).zfill(3)
-                            + ".png", seed) for i in range(9)]
+        vertical = [self.load_img(self.get_image_path(rand_scene, i + 4), seed) for i in range(0, 81, 9)]
+        horizontal = [self.load_img(self.get_image_path(rand_scene, i + 36), seed) for i in range(9)]
+        diagonal = [self.load_img(self.get_image_path(rand_scene, i * 10), seed) for i in range(9)]
 
-        return {'x1': torch.cat(x1, dim=0), 'x2': torch.cat(x2, dim=0), 'x3': torch.cat(x3, dim=0)}
+        return {'x1': torch.cat(vertical, dim=0), 'x2': torch.cat(horizontal, dim=0), 'x3': torch.cat(diagonal, dim=0)}
 
     def __len__(self):
         return self.length
+
+    def get_1hot_(self, label):
+        hot1 = np.zeros(self.n_classes)
+        hot1[label] = 1
+        return hot1
